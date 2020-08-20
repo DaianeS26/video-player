ef92b088f916389dc91c5016764d379a63ebaa0b
Daianes26 <daianeasilva26@gmail.com>
2020-08-19 23:39:37 -0400

    togglefullscreen function update

--- script.js
+++ script.js
@@ -132,11 +132,7 @@ function openFullscreen(elem) {
   let fullscreen = false;
 
   function toggleFullscreen(){
-    if(!fullscreen) {
-        openFullscreen(player);
-    } else {
-        closeFullscreen();
-    }
+    !fullscreen ? openFullscreen(player) : closeFullscreen();
     fullscreen = !fullscreen;
   }
 
